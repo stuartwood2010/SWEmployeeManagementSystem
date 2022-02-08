42669345334add96a2c62ef9700c65761fba9de0
Stuart Wood <stuartwood2010@hotmail.com>
2022-02-07 21:37:54 -0700

    Seed Database

--- db/schema.sql
+++ db/schema.sql
@@ -4,19 +4,19 @@ CREATE DATABASE management_db;
 USE management_db;
 
 CREATE TABLE department (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL,
     department_id INT NOT NULL
 );
 
 CREATE TABLE employee (
-    id INT PRIMARY KEY NOT NULL,
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
--- db/seeds.sql
+++ db/seeds.sql
@@ -1,26 +1,26 @@
-INSERT INTO department (id, name)
-VALUES  (001, "Sales"),
-        (002, "Engineering"),
-        (003, "Finance"),
-        (004, "Legal");
+INSERT INTO department (name)
+VALUES  ("Sales"),
+        ("Engineering"),
+        ("Finance"),
+        ("Legal");
 
-INSERT INTO roles (id, title, salary)
-VALUES  (001, "Sales Lead", 100000),
-        (002, "Salesperson", 80000),
-        (003, "Lead Engineer", 150000),
-        (004, "Software Engineer", 120000),
-        (005, "Account Manager", 1600000),
-        (006, "Accountant", 125000),
-        (007, "Legal Team Lead", 250000),
-        (008, "Lawyer", 190000);
+INSERT INTO roles (title, salary)
+VALUES  ("Sales Lead", 100000),
+        ("Salesperson", 80000),
+        ("Lead Engineer", 150000),
+        ("Software Engineer", 120000),
+        ("Account Manager", 1600000),
+        ("Accountant", 125000),
+        ("Legal Team Lead", 250000),
+        ("Lawyer", 190000);
 
-INSERT INTO roles (id, first_name, last_name)
-VALUES  (001, "John", "Doe"),
-        (002, "Mike", "Chan"),
-        (003, "Ashley", "Rodriguez"),
-        (004, "Kevin", "Tupik"),
-        (005, "Kunal", "Singh"),
-        (006, "Malia", "Brown"),
-        (007, "Sarah", "Lourd"),
-        (008, "Tom", "Allen");
+INSERT INTO roles (first_name, last_name)
+VALUES  ("John", "Doe"),
+        ("Mike", "Chan"),
+        ("Ashley", "Rodriguez"),
+        ("Kevin", "Tupik"),
+        ("Kunal", "Singh"),
+        ("Malia", "Brown"),
+        ("Sarah", "Lourd"),
+        ("Tom", "Allen");
         
