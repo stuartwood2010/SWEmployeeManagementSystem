c75f64461e8b26b1ff7c046f84557939a36db150
Stuart Wood <stuartwood2010@hotmail.com>
2022-02-19 22:12:11 -0700

    Inquirer started

--- server.js
+++ server.js
@@ -23,18 +23,143 @@ const db = mysql.createConnection(
   },
   console.log(`Connected to the management_db database.`)
 );
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT);
 
-inquirer.prompt
-// // Query database
-db.query('SELECT * FROM employee', function (err, results) {
-  console.table(results);
-});
+// const allDepartments = [];
+// const allRoles = [];
+// const allEmployees = [];
+
+// db.query('SELECT name FROM department', function (err, results) {
+//   allDepartments.push(results)
+//   console.log(allDepartments);
+// })
+
+const promptMenu = [
+  {
+    type: 'list',
+    message: 'What would you like to do?',
+    name: 'mainMenu',
+    choices: [
+      'View All Employees',
+      'Add Employee',
+      'Update Employee Role',
+      'View All Roles',
+      'Add Role',
+      'View All Departments',
+      'Add Department',
+      'Quit'
+    ]
+  }
+];
+const promptAddDept = [
+  {
+    type: 'input',
+    message: 'What is the name of the Department?',
+    name: 'departmentName',
+    validate: (value) => { if (value) {return true} else {return 'Cannot leave blank'}},
+  }
+];
+const promptAddRole = [
+  {
+    type: 'input',
+    message: 'What is the name of the Role?',
+    name: 'roleName',
+    validate: (value) => { if (value) {return true} else {return 'Cannot leave blank'}},
+  },
+  {
+    type: 'input',
+    message: 'What is the salary of the Role?',
+    name: 'roleSalary',
+    validate: (value) => { if (value) {return true} else {return 'Cannot leave blank'}},
+  },
+  {
+    type: 'list',
+    message: 'Which department does the role belong to?',
+    name: 'roleDept',
+    choices: [
+     
+    ]
+
+  }
+];
+const promptAddEmployee = [
+  {
+    type: 'input',
+    message: 'What is the employees first name?',
+    name: 'employeeFirstName',
+    validate: (value) => { if (value) {return true} else {return 'Cannot leave blank'}},
+  },
+  {
+    type: 'input',
+    message: 'What is the employees last name?',
+    name: 'employeeLastName',
+    validate: (value) => { if (value) {return true} else {return 'Cannot leave blank'}},
+  },
+  {
+    type: 'list',
+    message: 'What is the employees role of the employee?',
+    name: 'employeeRole',
+    choices: [
+
+    ]
 
 
+  }
 
+];
 
+function init() {
+  inquirer.prompt(promptMenu)
+    .then((data) => {
+      if (data.mainMenu === 'View All Employees') {
+        db.query('SELECT * FROM employee', function (err, results) {
+          console.table(results)
+        })
+        init()
+      }
+      else if (data.mainMenu === 'View All Roles') {
+        db.query('SELECT * FROM role', function (err, results) {
+          console.table(results)
+        })
+        init()
+      }
+      else if (data.mainMenu === 'View All Departments') {
+        db.query('SELECT * FROM department', function (err, results) {
+          console.table(results)
+        })
+        init()
+      }
+      else if (data.mainMenu === 'Add Employee') {
+        addEmployee()
+      }
+      else if (data.mainMenu === 'Add Role') {
+        addRole()
+      }
+      else if (data.mainMenu === 'Add Department') {
+        addDepartment()
+      }
+    })
+};
 
+function addEmployee() {
+  inquirer.prompt(promptAddEmployee)
+    .then((data) => {
 
+    })
+};
+
+function addRole() {
+  inquirer.prompt(promptAddRole)
+  .then((data) => {
+
+  })
+};
+
+function addDepartment() {
+  inquirer.prompt(promptAddDept)
+  .then((data) => {
+
+  })
+};
+
+init()
